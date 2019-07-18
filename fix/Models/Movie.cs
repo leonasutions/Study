b330c1c96de9a630ece12e06b5409b7574805ef4
leonasutions <leonasutions@gmail.com>
2019-07-18 08:32:09 +0700

    ErrorCreateNewMoviesFixed

--- fix/Models/Movie.cs
+++ fix/Models/Movie.cs
@@ -20,10 +20,10 @@ namespace fix.Models
         [Required]
         public byte GenreId { get; set; }
 
-        public DateTime DateAdded { get; set; }
+        public DateTime? DateAdded { get; set; }
 
         [Display(Name = "Release Date")]
-        public DateTime ReleaseDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
 
         [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
